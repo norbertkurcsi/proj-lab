324f1efff4d5f0308658fc494cda5920e88a7488
Zalán Barabási <zalanbarabasi@Zalans-MacBook-Pro.local>
2023-04-13 22:57:59 +0200

    Zalan skeleton modositas

--- 6. Szkeleton elkeszitese/DrukmakoriSivatag/src/Pump.java
+++ 6. Szkeleton elkeszitese/DrukmakoriSivatag/src/Pump.java
@@ -1,2 +1,28 @@
-public class Pump {
+public class Pump extends Field implements Tickable{
+    private boolean isBroken;
+    private int maxVolume;
+    private int currentVolume;
+
+    public void breakPump() {
+
+    }
+
+    public int addVolume(int amount) {
+        return 0;
+    }
+    public int decreaseVolume(int amount) {
+        return 0;
+    }
+    public void changeFlow(Pipe in, Pipe out) {
+
+    }
+    public void repair() {
+
+    }
+    public boolean addPlayer(Player p) {
+        return false;
+    }
+    public void tick() {
+
+    }
 }
