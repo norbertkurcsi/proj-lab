0d9a2db816199e6034e22ec8455111937fbc3ea9
andraspalasti <andraspalasti2@gmail.com>
2023-04-15 19:41:44 +0200

    Norbi pumpája

--- DrukmakoriSivatag/src/Pump.java
+++ DrukmakoriSivatag/src/Pump.java
@@ -1,4 +1,4 @@
-public class Pump extends FieldNode implements Tickable{
+public class Pump extends FieldNode implements Tickable {
     private Pipe pipeIn;
     private Pipe pipeOut;
     private boolean isBroken;
@@ -7,41 +7,60 @@ public class Pump extends FieldNode implements Tickable{
 
     /**
      * Konstruktor
-     * @param maxVolume - a pumpa maximális kapacítása
      */
-    public Pump(int maxVolume) {
-        this.isBroken = false;
-        this.maxVolume = maxVolume;
-        this.currentVolume = 0;
+    public Pump() {
+        Skeleton.callFunction(this, "create", null);
+        Skeleton.endFunction();
     }
 
     /**
      * A pumpa meghibásodik.
      */
     public void breakPump() {
-        this.isBroken = true;
+        ;
+        Skeleton.callFunction(this, "breakPump", null);
+        Skeleton.endFunction();
     }
 
-    public int addVolume(int amount) {
-        if((this.currentVolume + amount) > maxVolume) {
-            throw new RuntimeException("Meghaladtad a maximális mennyiséget!");
-        }
-        return this.currentVolume + amount;
+    public int addVolume(Integer amount) {
+        Skeleton.callFunction(this, "addVolume", new Object[] { amount });
+        int volume = Skeleton.numberQuestion("How much will be the current volume?");
+        Skeleton.endFunction();
+        return volume;
     }
+
     public int decreaseVolume(int amount) {
-        return this.currentVolume - amount;
+        Skeleton.callFunction(this, "decreaseVolume", new Object[] { amount });
+        int volume = Skeleton.numberQuestion("How much will be the current volume?");
+        Skeleton.endFunction();
+        return volume;
     }
+
     public void changeFlow(Pipe in, Pipe out) {
-
+        pipeIn = in;
+        pipeOut = out;
+        Skeleton.callFunction(this, "changeFlow", new Object[] { in, out });
+        Skeleton.endFunction();
     }
+
     public void repair() {
-        this.isBroken = true;
+        Skeleton.callFunction(this, "repair", null);
+        Skeleton.endFunction();
     }
-    public boolean addPlayer(Player p) {
-        return false;
-    }
-    public void tick() {
 
+    public void tick() {
+        Skeleton.callFunction(this, "tick", null);
+        boolean isBroken = Skeleton.yesNoQuestion("Is the pump broken?");
+        if (isBroken) {
+            Skeleton.endFunction();
+            return;
+        }
+
+        int amountDrain = Skeleton.numberQuestion("How much do you want to pump?");
+        int gotWater = pipeIn.drain(amountDrain);
+        pipeOut.flow(gotWater);
+
+        Skeleton.endFunction();
     }
 
 }
